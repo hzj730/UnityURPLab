cbe03def885f1e67eb40e89176f9bbefb45fb648
jackey7 <hzjtyh@gmail.com>
2021-04-13 11:47:21 +0800

    custom-render-pipeline tutorials

---
+++ CustomSRP/Assets/Runtime/CustomRenderPipelineAsset.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/Custom Render Pipeline")]
+public class CustomRenderPipelineAsset : RenderPipelineAsset
+{
+    protected override RenderPipeline CreatePipeline()
+    {
+        //throw new System.NotImplementedException();
+        return new CustomRenderPipeline();
+    }
+}
